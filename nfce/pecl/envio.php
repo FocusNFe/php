52e925b1d8531960ca7536f06a3ae60746db57b4
Guilherme-Esplugues <guilherme.esplugues@acras.com.br>
2017-07-28 12:04:34 -0300

    Exemplo de código para autorizar uma Nota Fiscal Eletrônica de Consumidor.

--- nfce/pecl/envio.php
+++ nfce/pecl/envio.php
@@ -10,42 +10,42 @@ $ref = "12345";
 
 $nfce = array (
     "natureza_operacao" => "Venda ao Consumidor",
-	"formas_pagamento" => array(
+    "formas_pagamento" => array(
+               array(
+               "forma_pagamento"=> "1",
+               "valor_pagamento"=> "1.00"
+                    )
+         ),
+    "forma_pagamento" => "0",
+    "tipo_documento" => "1",
+    "consumidor_final" => "1",
+    "finalidade_emissao" => "1",
+    "presenca_comprador" => "1",
+    "cnpj_emitente" => "51916585000125",
+    "cpf_destinatario" => "51966818092",
+    "nome_destinatario" => "NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL",
+    "items"=> array(
 		array(
-			"forma_pagamento"=> "1",
-			"valor_pagamento"=> "1.00"
-			)
-		),
-	"forma_pagamento" => "0", 
-	"tipo_documento" => "1", 
-	"consumidor_final" => "1", 
-	"finalidade_emissao" => "1",
-	"presenca_comprador" => "1",
-	"cnpj_emitente" => "51916585000125",
-	"cpf_destinatario" => "51966818092",
-	"nome_destinatario" => "NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL",
-	"items"=> array(
-		array(
-			"numero_item"=> "1",
-			"codigo_produto"=> "293656",
-			"descricao"=> "NOTA FISCAL EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL",
-			"codigo_ncm"=> "39269090",
-			"cfop"=> "5101",
-			"unidade_comercial"=> "PC",
-			"valor_unitario_comercial"=> "1.00",
-			"valor_bruto"=> "1.00",
-			"unidade_tributavel"=> "PC",
-			"quantidade_tributavel"=> "1.00",
-			"quantidade_comercial"=> "1.00",
-			"valor_unitario_tributavel"=> "1.00",
-			"icms_origem"=> "0",
-			"icms_situacao_tributaria"=> "102"
-			)
-		),
-	"valor_produtos" => "1.00",
-	"valor_total" => "1.00",
-	"data_emissao" => "2017-07-25T17:28:22-03:00",
-	"modalidade_frete" => "9"
+		"numero_item"=> "1",
+		"codigo_produto"=> "293656",
+		"descricao"=> "NOTA FISCAL EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL",
+		"codigo_ncm"=> "39269090",
+		"cfop"=> "5101",
+		"unidade_comercial"=> "PC",
+		"valor_unitario_comercial"=> "1.00",
+		"valor_bruto"=> "1.00",
+		"unidade_tributavel"=> "PC",
+		"quantidade_tributavel"=> "1.00",
+		"quantidade_comercial"=> "1.00",
+		"valor_unitario_tributavel"=> "1.00",
+		"icms_origem"=> "0",
+		"icms_situacao_tributaria"=> "102"
+		)
+        ),
+     "valor_produtos" => "1.00",
+     "valor_total" => "1.00",
+     "data_emissao" => "2017-07-25T17:28:22-03:00",
+     "modalidade_frete" => "9"
 );
 
 // Transforma a váriavel em objeto JSON.
