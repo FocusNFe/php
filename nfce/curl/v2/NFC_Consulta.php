e8c4e957f9d73556f4716781217bf0056cc74fdf
Luciano Romaniecki <30294939+Romaniecki@users.noreply.github.com>
2017-12-07 13:33:02 -0200

    Código em PHP para NFC-e no modelo V2

---
+++ nfce/curl/v2/NFC_Consulta.php
@@ -0,0 +1,25 @@
+
+<?php
+// Você deve definir isso globalmente para sua aplicação
+//Substituir pela sua identificação interna da nota
+$ref = "12345";
+$login = "token_enviado_pelo_suporte";
+$password = "";
+// Para ambiente de produção use a variável abaixo:
+// $server = "https://api.focusnfe.com.br/nfe2/";
+$server = "http://homologacao.acrasnfe.acras.com.br"; // Servidor de homologação
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $server."/v2/nfce/" . $ref);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, "$login:$password");
+$body = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//as três linhas abaixo imprimem as informações retornadas pela API, aqui o seu sistema deverá
+//interpretar e lidar com o retorno
+print($http_code."\n");
+print($body."\n\n");
+print("");
+curl_close($ch);
+?>
